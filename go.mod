de085cb3c63255906c2e92ec3bb47c69b49b6b1e
SrWither <fjachuf@gmail.com>
2023-01-06 13:56:46 -0300

    add cors to frontend

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.19
 
 require (
 	github.com/99designs/gqlgen v0.17.22
+	github.com/go-chi/chi v4.0.4+incompatible
 	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365
 	github.com/joho/godotenv v1.4.0
 	github.com/prisma/prisma-client-go v0.16.3
+	github.com/rs/cors v1.8.0
 	github.com/shopspring/decimal v1.3.1
 	github.com/takuoki/gocase v1.0.0
 	github.com/vektah/gqlparser/v2 v2.5.1
--- server.go
+++ server.go
@@ -1,28 +1,44 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 	"pruebas/graph"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://192.168.60.188:8000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("Starwars", "/query"))
+	router.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	print("localhost:8080")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		panic(err)
+	}
+
 }
